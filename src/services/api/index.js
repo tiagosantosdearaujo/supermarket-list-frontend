bb5ba3b6a541418257f88c5e758a882ea04d6d9e
Tiago Araujo <tiago.tsa1@hotmail.com>
2023-02-24 09:53:57 -0800

    update: change API URL

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { SAVE_USERNAME_PATH } from 'services/constants'
 
 export const api = axios.create({
-  baseURL: 'https:supermarketlist-api.herokuapp.com',
+  baseURL: 'https://supermarket-list-web-backend.herokuapp.com',
   headers: {
     username: localStorage.getItem(SAVE_USERNAME_PATH) || ''
   }
